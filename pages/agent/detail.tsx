2da317fd9d00c9e9142fc7eb60608721a904ab99
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-07-23 20:17:09 +0900

    feat: modify dealer page

--- pages/agent/detail.tsx
+++ pages/agent/detail.tsx
@@ -198,10 +198,7 @@ const AgentDetail: NextPage = ({ initialInput, initialComment, ...props }: any) 
 			<Stack className={'agent-detail-page'}>
 				<Stack className={'container'}>
 					<Stack className={'agent-info'}>
-						<img
-							src={agent?.memberImage ? `${REACT_APP_API_URL}/${agent?.memberImage}` : '/img/profile/defaultUser.svg'}
-							alt=""
-						/>
+						<img src={agent?.memberImage ? `${REACT_APP_API_URL}/${agent?.memberImage}` : ''} alt="" />
 						<Box component={'div'} className={'info'} onClick={() => redirectToMemberPageHandler(agent?._id as string)}>
 							<strong>{agent?.memberFullName ?? agent?.memberNick}</strong>
 							<div>
@@ -237,7 +234,7 @@ const AgentDetail: NextPage = ({ initialInput, initialComment, ...props }: any) 
 										/>
 									</Stack>
 									<span>
-										Total {propertyTotal} propert{propertyTotal > 1 ? 'ies' : 'y'} available
+										Total {propertyTotal} vehicle{propertyTotal > 1 ? 's' : ''} 
 									</span>
 								</>
 							) : (
@@ -250,8 +247,8 @@ const AgentDetail: NextPage = ({ initialInput, initialComment, ...props }: any) 
 					</Stack>
 					<Stack className={'review-box'}>
 						<Stack className={'main-intro'}>
-							<span>Reviews</span>
-							<p>we are glad to see you again</p>
+							<span>Assessments</span>
+							<p>We're delighted to see you again</p>
 						</Stack>
 						{commentTotal !== 0 && (
 							<Stack className={'review-wrap'}>
@@ -296,7 +293,7 @@ const AgentDetail: NextPage = ({ initialInput, initialComment, ...props }: any) 
 										<g clipPath="url(#clip0_6975_3642)">
 											<path
 												d="M16.1571 0.5H6.37936C6.1337 0.5 5.93491 0.698792 5.93491 0.944458C5.93491 1.19012 6.1337 1.38892 6.37936 1.38892H15.0842L0.731781 15.7413C0.558156 15.915 0.558156 16.1962 0.731781 16.3698C0.818573 16.4566 0.932323 16.5 1.04603 16.5C1.15974 16.5 1.27345 16.4566 1.36028 16.3698L15.7127 2.01737V10.7222C15.7127 10.9679 15.9115 11.1667 16.1572 11.1667C16.4028 11.1667 16.6016 10.9679 16.6016 10.7222V0.944458C16.6016 0.698792 16.4028 0.5 16.1571 0.5Z"
-												fill="#181A20"
+												fill="#0f65d6"
 											/>
 										</g>
 										<defs>
