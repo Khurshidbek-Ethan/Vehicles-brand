ad66530488fdf10e9001222193ca054a0e04f4b2
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-07-23 16:16:48 +0900

    feat:create Detaile page

--- libs/components/common/PropertyBigCard.tsx
+++ libs/components/common/PropertyBigCard.tsx
@@ -43,32 +43,28 @@ const PropertyBigCard = (props: PropertyBigCardProps) => {
 							<span>top</span>
 						</div>
 					)}
-
-					<div className={'price'}>${formatterStr(property?.propertyPrice)}</div>
 				</Box>
 				<Box component={'div'} className={'info'}>
 					<strong className={'title'}>{property?.propertyTitle}</strong>
 					<p className={'desc'}>{property?.propertyAddress}</p>
 					<div className={'options'}>
 						<div>
-							<img src="/img/icons/bed.svg" alt="" />
-							<span>{property?.propertyBeds} bed</span>
+							<img src="/img/icons/car.png" alt="" />
+							<span>{property?.propertyBrand} </span>
 						</div>
 						<div>
-							<img src="/img/icons/room.svg" alt="" />
-							<span>{property?.propertyRooms} rooms</span>
+							<img src="/img/icons/colorcar.png" alt="" />
+							<span>{property?.propertyColor} </span>
 						</div>
 						<div>
-							<img src="/img/icons/expand.svg" alt="" />
-							<span>{property?.propertySquare} m2</span>
+							<img src="/img/icons/colcar.png" alt="" />
+							<span>{property?.propertyType}</span>
 						</div>
 					</div>
 					<Divider sx={{ mt: '15px', mb: '17px' }} />
 					<div className={'bott'}>
-						<div>
-							{property?.propertyRent ? <p>Rent</p> : <span>Rent</span>}
-							{property?.propertyBarter ? <p>Barter</p> : <span>Barter</span>}
-						</div>
+						<div className={'price'}>${formatterStr(property?.propertyPrice)}</div>
+
 						<div className="buttons-box">
 							<IconButton color={'default'}>
 								<RemoveRedEyeIcon />
