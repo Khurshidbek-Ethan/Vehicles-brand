d7882ded7548f2bf0acdcfb1d4690f35497981c0
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-08-16 09:21:28 +0900

    feat:delete BMW file

--- libs/components/homepage/VehicleHyundai.tsx
+++ libs/components/homepage/VehicleHyundai.tsx
@@ -7,7 +7,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Pagination } from 'swiper';
 import { Property } from '../../types/property/property';
 import { PropertiesInquiry } from '../../types/property/property.input';
-
 import { useMutation, useQuery } from '@apollo/client';
 import { GET_PROPERTIES, GET_PROPERTY } from '../../../apollo/user/query';
 import { T } from '../../types/common';
